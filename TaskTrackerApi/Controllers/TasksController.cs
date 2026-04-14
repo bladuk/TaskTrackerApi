26e18b6a90bd3622cfce2d111bfc7614ea4943cd
Vladislav Panikorovskii <fategpan764@gmail.com>
2026-04-14 20:59:41 +0300

    feat: implement task CRUD API

---
+++ TaskTrackerApi/Controllers/TasksController.cs
@@ -0,0 +1,49 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+using TaskTrackerApi.DTO.Tasks;
+using TaskTrackerApi.Services.Interfaces;
+
+namespace TaskTrackerApi.Controllers;
+
+[ApiController]
+[ApiVersion("1.0")]
+[Route("[controller]")]
+[Produces("application/json")]
+public class TasksController(ITaskService taskService) : ControllerBase
+{
+    [HttpGet]
+    [ProducesResponseType(typeof(IReadOnlyList<TaskItemDto>), StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetTasks([FromQuery] bool? isCompleted, [FromQuery] Guid? projectId, CancellationToken ct = default)
+    {
+        return Ok(await taskService.GetTasksAsync(isCompleted, projectId, ct));
+    }
+
+    [HttpGet("{id:guid}")]
+    [ProducesResponseType(typeof(TaskItemDto), StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetTaskById([FromRoute] Guid id, CancellationToken ct = default)
+    {
+        return Ok(await taskService.GetTaskByIdAsync(id, ct));
+    }
+    
+    [HttpPost]
+    [ProducesResponseType(typeof(TaskItemDto), StatusCodes.Status201Created)]
+    public async Task<IActionResult> CreateTask([FromBody] CreateTaskDto data, CancellationToken ct = default)
+    {
+        return Created("", await taskService.CreateTaskAsync(data, ct));
+    }
+
+    [HttpPut("{id:guid}")]
+    [ProducesResponseType(typeof(TaskItemDto), StatusCodes.Status200OK)]
+    public async Task<IActionResult> UpdateTask([FromRoute] Guid id, [FromBody] UpdateTaskDto data, CancellationToken ct = default)
+    {
+        return Ok(await taskService.UpdateTaskAsync(id, data, ct));
+    }
+    
+    [HttpDelete("{id:guid}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public async Task<IActionResult> DeleteTask([FromRoute] Guid id, CancellationToken ct = default)
+    {
+        await taskService.DeleteTaskAsync(id, ct);
+        return NoContent();
+    }
+}
---
+++ TaskTrackerApi/Validators/UpdateTaskDtoValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using TaskTrackerApi.DTO.Tasks;
+
+namespace TaskTrackerApi.Validators;
+
+public class UpdateTaskDtoValidator : AbstractValidator<UpdateTaskDto>
+{
+    public UpdateTaskDtoValidator()
+    {
+        RuleFor(x => x.Title)
+            .NotEmpty().WithMessage("Task title could not be empty")
+            .MaximumLength(255).WithMessage("Task title could not be longer than 255 characters");
+        
+        RuleFor(x => x.Description)
+            .MaximumLength(1000).WithMessage("Task description could not be longer than 1000 characters");
+    }
+}
