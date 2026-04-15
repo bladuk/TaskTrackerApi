e9f2dfb2a1243ffbdb8a4d53c4047802a03c1aa4
Vladislav Panikorovskii <fategpan764@gmail.com>
2026-04-15 16:05:28 +0300

    feat: enforce authorization for task and project endpoints

--- TaskTrackerApi/Controllers/TasksController.cs
+++ TaskTrackerApi/Controllers/TasksController.cs
@@ -1,4 +1,5 @@
 ﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using TaskTrackerApi.DTO.Tasks;
 using TaskTrackerApi.Services.Interfaces;
@@ -24,7 +25,8 @@ public class TasksController(ITaskService taskService) : ControllerBase
     {
         return Ok(await taskService.GetTaskByIdAsync(id, ct));
     }
-    
+
+    [Authorize]
     [HttpPost]
     [ProducesResponseType(typeof(TaskItemDto), StatusCodes.Status201Created)]
     public async Task<IActionResult> CreateTask([FromBody] CreateTaskDto data, CancellationToken ct = default)
@@ -32,13 +34,15 @@ public class TasksController(ITaskService taskService) : ControllerBase
         return Created("", await taskService.CreateTaskAsync(data, ct));
     }
 
+    [Authorize]
     [HttpPut("{id:guid}")]
     [ProducesResponseType(typeof(TaskItemDto), StatusCodes.Status200OK)]
     public async Task<IActionResult> UpdateTask([FromRoute] Guid id, [FromBody] UpdateTaskDto data, CancellationToken ct = default)
     {
         return Ok(await taskService.UpdateTaskAsync(id, data, ct));
     }
-    
+
+    [Authorize]
     [HttpDelete("{id:guid}")]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
     public async Task<IActionResult> DeleteTask([FromRoute] Guid id, CancellationToken ct = default)
