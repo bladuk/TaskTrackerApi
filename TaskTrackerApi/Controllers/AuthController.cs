4d532d5c9a490653c5cc8a67a8e1415b8e37c016
Vladislav Panikorovskii <fategpan764@gmail.com>
2026-04-15 15:59:39 +0300

    feat: add AuthController with login and register endpoints

---
+++ TaskTrackerApi/Controllers/AuthController.cs
@@ -0,0 +1,25 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+using TaskTrackerApi.DTO.Auth;
+using TaskTrackerApi.Services.Interfaces;
+
+namespace TaskTrackerApi.Controllers;
+
+[ApiController]
+[ApiVersion("1.0")]
+[Route("[controller]")]
+[Produces("application/json")]
+public class AuthController(IAuthService authService) : ControllerBase
+{
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] LoginDto loginDto, CancellationToken ct = default)
+    {
+        return Ok(await authService.LoginAsync(loginDto, ct));
+    }
+    
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody] RegisterDto registerDto, CancellationToken ct = default)
+    {
+        return Ok(await authService.RegisterAsync(registerDto, ct));
+    }
+}
