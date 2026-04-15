832280d0bc948b462edd55a84a88c8becead8973
Vladislav Panikorovskii <fategpan764@gmail.com>
2026-04-15 15:26:07 +0300

    feat: introduce user repository

---
+++ TaskTrackerApi/Repositories/UserRepository.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskTrackerApi.Data;
+using TaskTrackerApi.Models;
+using TaskTrackerApi.Repositories.Abstractions;
+using TaskTrackerApi.Repositories.Interfaces;
+
+namespace TaskTrackerApi.Repositories;
+
+public class UserRepository(TaskTrackerDbContext context) : RepositoryBase<User, Guid>(context), IUserRepository
+{
+    public async Task<User?> GetByUsernameAsync(string username, CancellationToken ct = default) =>
+        await Entities.FirstOrDefaultAsync(u => u.Username == username, ct);
+}
