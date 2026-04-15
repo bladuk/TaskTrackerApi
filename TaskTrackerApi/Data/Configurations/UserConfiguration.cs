39742eb683a654f37287eb914e67b03b264b22b1
Vladislav Panikorovskii <fategpan764@gmail.com>
2026-04-15 15:07:19 +0300

    feat: add user entity and related database migration

---
+++ TaskTrackerApi/Data/Configurations/UserConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TaskTrackerApi.Models;
+
+namespace TaskTrackerApi.Data.Configurations;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.HasKey(p => p.Id);
+        builder.Property(p => p.Username).IsRequired().HasMaxLength(255);
+        builder.Property(p => p.Password).IsRequired().HasMaxLength(255);
+        builder.HasIndex(p => p.Username).IsUnique();
+    }
+}
--- TaskTrackerApi/Data/TaskTrackerDbContext.cs
+++ TaskTrackerApi/Data/TaskTrackerDbContext.cs
@@ -9,6 +9,8 @@ public class TaskTrackerDbContext(DbContextOptions<TaskTrackerDbContext> options
     
     public DbSet<TaskItem> Tasks => Set<TaskItem>();
 
+    public DbSet<User> Users => Set<User>();
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
