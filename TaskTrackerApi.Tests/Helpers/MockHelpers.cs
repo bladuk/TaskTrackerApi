4ccd6be8f60e3c0dbc8c65446b0a541782c85525
Vladislav Panikorovskii <fategpan764@gmail.com>
2026-04-16 12:10:31 +0300

    test: add unit tests for ProjectsController

---
+++ TaskTrackerApi.Tests/Helpers/MockHelpers.cs
@@ -0,0 +1,34 @@
+﻿using TaskTrackerApi.DTO.Common;
+using TaskTrackerApi.DTO.Projects;
+using TaskTrackerApi.DTO.Tasks;
+
+namespace TaskTrackerApi.Test.Helpers;
+
+public static class MockHelpers
+{
+    public static ProjectDto SampleProjectDto(Guid? id = null, string name = "Sample Project", string description = "Sample Description") =>
+        new(id ?? Guid.NewGuid(), name, description, DateTime.UtcNow, DateTime.UtcNow);
+
+    public static PagedResult<ProjectDto> SamplePagedProjectsDto(int count = 3) => new(
+        Data: Enumerable.Range(0, count).Select(i => SampleProjectDto()).ToList(),
+        Meta: new PagedResultMeta(1, 10, count)
+    );
+    
+    public static TaskItemDto SampleTaskDto(Guid? id = null, Guid? projectId = null) =>
+        new(id ?? Guid.NewGuid(), "Sample Task", "Sample Description", false, DateTime.UtcNow, DateTime.UtcNow, projectId ?? Guid.NewGuid());
+
+    public static ProjectWithTasksDto SampleProjectWithTasksDto(Guid? id = null)
+    {
+        Guid projectId = id ?? Guid.NewGuid();
+        return new(projectId, "Sample Project", "Sample Description", DateTime.UtcNow, DateTime.UtcNow,
+        [
+            SampleTaskDto(Guid.NewGuid(), projectId)
+        ]);
+    }
+    
+    public static CreateProjectDto SampleCreateProjectDto() => 
+        new("Sample Project", "Sample Description");
+    
+    public static UpdateProjectDto SampleUpdateProjectDto() =>
+        new("Updated Name", "Updated Description");
+}
